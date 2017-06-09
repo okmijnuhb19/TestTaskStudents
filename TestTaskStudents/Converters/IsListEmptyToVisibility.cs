6fde19a0bd0ff2c9480495c8f6a6830bf24f588d
ITRANSITION/a.melnikov <a.melnikov@itransition.com>
2017-06-09 10:53:51 +0300

    emplty list logic has been added

--- TestTaskStudents/Converters/IsListEmptyToVisibility.cs
+++ TestTaskStudents/Converters/IsListEmptyToVisibility.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Windows;
 using System.Windows.Data;
+using TestTaskStudents.Models;
 
 namespace TestTaskStudents.Converters
 {
-    public class IsNullConverter : IValueConverter
+    public class IsListEmptyToVisibility : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null;
+            var list = (List<Student>) value;
+            return list != null && list.Any() ? Visibility.Hidden : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ TestTaskStudents/Converters/IsListViewVisible.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using TestTaskStudents.Models;
+
+namespace TestTaskStudents.Converters
+{
+    public class IsListViewVisible : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = (List<Student>)value;
+            return list != null && list.Any() ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
